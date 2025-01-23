6773a5c2d985e8a6298fd8343249c1cf5ac50a05
xristosgala <100777920+xristosgala@users.noreply.github.com>
2025-01-23 18:34:22 +0200

    Update workforce_planning.py

--- workforce_planning.py
+++ workforce_planning.py
@@ -44,11 +44,11 @@ def solve_workforce_planning(weeks, hiring_cost, firing_cost, salary_cost, penal
     # Solve the problem
     problem.solve()
 
-    objective_cost = problem.objective.value() - U[i] * penalty_cost
-                                
+    for i in range(weeks):
+        objective_cost = U[i] * penalty_cost
     results = {
         "Status": LpStatus[problem.status],
-        "Total Cost": objective_cost,
+        "Total Cost": problem.objective.value() * objective_cost,
         "Details": []
     }
 
