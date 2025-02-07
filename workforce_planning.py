b74c5ebedc90752f1f5050e9c42a1e692e6f9246
xristosgala <100777920+xristosgala@users.noreply.github.com>
2025-02-07 19:13:15 +0200

    Update workforce_planning.py

--- workforce_planning.py
+++ workforce_planning.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import random
-from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpStatus
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpStatus, PULP_CBC_CMD
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
