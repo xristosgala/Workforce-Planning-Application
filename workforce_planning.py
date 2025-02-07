49aea44c73aa904f54264fc1988f4a88d1ecf1c3
xristosgala <100777920+xristosgala@users.noreply.github.com>
2025-02-07 14:38:59 +0200

    Update workforce_planning.py

--- workforce_planning.py
+++ workforce_planning.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import random
-from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpStatus, PULP_CBC_CMD
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpStatus, PULP_GLPK_CMD
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import pulp
 
 def solve_workforce_planning(weeks, hiring_cost, firing_cost, salary_cost, penalty_cost,
                               overtime_cost, initial_employees, maxh, maxf, overtime_rate,
@@ -43,7 +42,7 @@ def solve_workforce_planning(weeks, hiring_cost, firing_cost, salary_cost, penal
     problem += total_cost <= budget, "Budget_Constraint"
 
     # Solve the problem
-    problem.solve(PULP_CBC_CMD(msg=True))
+    problem.solve(PULP_GLPK_CMD(msg=True))
 
     objective_cost = 0
     for i in range(weeks):
