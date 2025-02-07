37daf370df25f304fdc1ac37f804f6024f2e2d0f
xristosgala <100777920+xristosgala@users.noreply.github.com>
2025-02-07 14:35:37 +0200

    Update workforce_planning.py

--- workforce_planning.py
+++ workforce_planning.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import random
-from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpStatus
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpStatus, PULP_CBC_CMD
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import pulp
-import PULP_CBC_CMD
 
 def solve_workforce_planning(weeks, hiring_cost, firing_cost, salary_cost, penalty_cost,
                               overtime_cost, initial_employees, maxh, maxf, overtime_rate,
